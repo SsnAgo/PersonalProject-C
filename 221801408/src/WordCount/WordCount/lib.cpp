63dc3006b95d3837d8eb602ffdabd0b98285714c
llk1233 <76471936+llk1233@users.noreply.github.com>
2021-02-26 12:04:34 +0800

    修改文件编码类型 转化为中文

--- 221801408/src/WordCount/WordCount/lib.cpp
+++ 221801408/src/WordCount/WordCount/lib.cpp
@@ -1,13 +1,13 @@
-#include "lib.h" 
+﻿#include "lib.h" 
 void WordCount::start()
 {
-	ifstream file(input_file);      //Read the current folder input.txt file
+	ifstream file(input_file);      //读取文件目录下的input.txt文件
     FILE *fp;
 	fopen_s(&fp, input_file, "r");
 	WordCount A(fp, input_file, output_file);
 	ofstream outfile(output_file, ios::out);
 	if (!outfile) {
-		cerr << "create error!" << endl;
+		cerr << "创建失败！" << endl;
 		exit(1);
 	}outfile.close();
 }
