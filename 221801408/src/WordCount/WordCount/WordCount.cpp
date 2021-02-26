ca3a6f1e431e2d5ff161b9417bb015912b133e81
llk1233 <76471936+llk1233@users.noreply.github.com>
2021-02-26 10:48:37 +0800

    功能：增加输入功能

--- 221801408/src/WordCount/WordCount/WordCount.cpp
+++ 221801408/src/WordCount/WordCount/WordCount.cpp
@@ -1,29 +1,40 @@
-﻿// WordCount.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-#include "lib.h"
-struct word {
-	char name[30];
-	int num;
-	struct word* next;
-};
+﻿#include<iterator>
+#include<cctype>
+#include<ctype.h>
+#include<unordered_map>
+#include<algorithm>
+#include<iostream>
 
-int main()
+
+
+using namespace std;
+
+int main(int argc, char *argv[])
 {
-	struct word* head;
-	head = new word;
-	head->next = NULL;
-	readfile(head);
-	sort(head);
+	const char* filename2;
+	const char* filename;
+
+	if (argc == 3)
+	{
+		filename = argv[1];
+		filename2 = argv[2];
+	}
+	else
+	{
+		cerr << "输入参数失败!" << endl;
+		exit(1);
+	}
+	FILE *fp;
+	errno_t err = fopen_s(&fp, filename, "r");
+	if (err != 0)
+	{
+		cerr << "打开失败!" << endl;
+		exit(1);
+	}
+	
+
+
+	fclose(fp);
+
 	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
