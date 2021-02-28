f93de26e4c9dd75826702e84a575c336ea262480
ftdkf <76504869+ftdkf@users.noreply.github.com>
2021-02-28 20:06:45 +0800

    WordCount
    计算有效单词数

---
+++ 221801417/src/wordNum.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+#include "wordNum.h"
+using namespace std;
+
+int wordNum(char * filename)//计算符合条件的单词总数
+{
+	ifstream file;
+	file.open(filename);
+	int count = 0;
+	string s;
+	regex r("\\b[a-zA-Z]{4}[a-zA-Z0-9]*", regex::icase);
+	while (file >> s) 
+	{
+		smatch m;
+		while (regex_search(s, m, r)) //搜索符合正则表达式的单词
+	    {
+			count++;
+			s = m.suffix().str();
+		}
+	}
+	file.close();
+
+	return count;
+}
