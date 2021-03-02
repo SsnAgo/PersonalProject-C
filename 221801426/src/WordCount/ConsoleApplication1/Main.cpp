ae824d213c5de3b321c8cf88b6dad035aae9b0dd
Lin <1220627779@qq.com>
2021-03-02 22:44:09 +0800

    统计行数

--- 221801426/src/WordCount/ConsoleApplication1/Main.cpp
+++ 221801426/src/WordCount/ConsoleApplication1/Main.cpp
@@ -4,11 +4,13 @@
 #include <stdio.h>
 #include <iostream>
 #include <cstring>
+#include <string>
 #include <fstream>
 
 using namespace std;
 
 int CharCount(const char* filename);
+int LinesCount(const char* filename);
 
 int main(int argc, const char* argv[]) {
 	const char* InputFileName = "input.txt";
@@ -19,9 +21,13 @@ int main(int argc, const char* argv[]) {
 	{
 		cout << "Erro!" << endl;
 	}
-	long CharNum = CharCount(InputFileName);
+	int CharNum = CharCount(InputFileName);
+	int LinesNum = LinesCount(InputFileName);
+
 	ofstream out(OutputFileName);
+
 	out << "characters:" << CharNum << endl;
+	out << "lines:" << LinesNum << endl;
 	out.close();
 	return 0;
 }
@@ -40,5 +46,28 @@ int CharCount(const char* filename)
 	ifs.clear();
 	ifs.seekg(0);
 	return CharNum;
+}
+
+int LinesCount(const char* filename)
+{
+	int i = 0;
+	bool isNull = 1;
+	int lines = 0;
+	fstream fs(filename, ios::in);
+	string s;
+	while (getline(fs, s))
+	{
+		for (i = 0, isNull = 1; i < s.length(); i++)
+		{
+			if (s[i] != ' ' && s[i] != '\t')
+			{
+				isNull = 0;
+				break;
+			}
+		}
+		if (!isNull)
+			lines++;
+	}
+	return lines;
 }
 
